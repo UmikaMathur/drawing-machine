9fa934c5cef381cd3b32aa94e9bc234d37ba370b
Umika Mathur <umika.mathur@sjsu.edu>
2020-03-25 16:48:22 -0700

    update
    update

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 let array = [];
-
+let backgroundColor = 296, 42, 49;
 
 function setup() {
   createCanvas(600, 600);
-  background(296, 42, 49);
+  background(backgroundColor);
 
   strokeWeight(5);
 
@@ -12,10 +12,21 @@ function setup() {
 function draw() {
   if (mouseIsPressed){
     background(0);
-    //stroke(map(mouseX, 0, 600, 0, 255, true))
-    //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-    //line(mouseX, mouseY, pmouseX, pmouseY,);
+    backgroundColor -= 5;
+    background(backgroundColor);
+
+
+    stroke(map(mouseX, 0, 600, 0, 255, true))
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY,);
     array.push([mouseX, mouseY]);
+
+    beginShape();
+    for(let i = 0; i < array.length; i++){
+      line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      console.log(i);
+    }
+    endShape();
   }
 }
 
@@ -26,16 +37,26 @@ function keyTyped(){
 
 //save this image
   saveCanvas('fileName', 'png');
-} else if (key === 'd'){
+}
+  else if (key === 'd'){
   //display image
   background(296, 42, 49);
+
+  beginShape();
   for(let i = 0; i < array.length; i++){
     line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
     console.log(i);
   }
-
+  endShape();
 
 }
 
   return false;
+}
+
+function mousePressed() {
+  console.log("mouse pressed function");
+  array = [];
+  console.log(array)
+  backgroundColor = 255;
 }
