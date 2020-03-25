a0a5a4b8f122e7bfbf60dd971987b596ea28d04d
Umika Mathur <umika.mathur@sjsu.edu>
2020-03-24 19:42:12 -0700

    updated
    updated

--- sketch.js
+++ sketch.js
@@ -1,7 +1,41 @@
+let array = [];
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(296, 42, 49);
+
+  strokeWeight(5);
+
 }
 
 function draw() {
-  background(220);
+  if (mouseIsPressed){
+    background(0);
+    //stroke(map(mouseX, 0, 600, 0, 255, true))
+    //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    //line(mouseX, mouseY, pmouseX, pmouseY,);
+    array.push([mouseX, mouseY]);
+  }
+}
+
+function keyTyped(){
+  console.log(`key ${key} is being typed`)
+  console.log(`key ${key} is being typed`)
+  if (key === 's'){
+
+//save this image
+  saveCanvas('fileName', 'png');
+} else if (key === 'd'){
+  //display image
+  background(296, 42, 49);
+  for(let i = 0; i < array.length; i++){
+    line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+    console.log(i);
+  }
+
+
+}
+
+  return false;
 }
